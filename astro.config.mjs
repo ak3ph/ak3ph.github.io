cc2f43ca7a52bcb9ae4363e443e022d3703e57e9
heylocalhost <yashcodedev@gmail.com>
2024-09-03 19:26:32 +0530

    did the requied setup for github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://astronaut.github.io",
   integrations: [
     tailwind(),
     sitemap({
@@ -17,3 +17,5 @@ export default defineConfig({
     robotsTxt(),
   ],
 });
+
+
