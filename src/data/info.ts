a130a9dcbe0a821fc69da35462045f38dd14c348
heylocalhost <yashcodedev@gmail.com>
2024-09-03 19:16:03 +0530

    init commit for base setup

---
+++ src/data/info.ts
@@ -0,0 +1,126 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Yash Thakur",
+  jobDescription: "DevOps Engineer",
+  about: `As a DevOps Engineer with 4+ years of experience, I specialize in optimizing CI/CD pipelines, implementing Infrastructure as Code (IaC), and architecting scalable cloud solutions. 
+          My expertise spans a comprehensive DevOps tech stack, including AWS, containerization with Docker and Kubernetes, and automation tools like Ansible and Terraform. 
+          I'm proficient in multiple programming languages including JavaScript and Python which I leverage for effective scripting and automation.   
+          My experience includes managing CI/CD pipelines using Jenkins, GitLab CI, and AWS CodePipeline, as well as optimizing AWS resources for cost-effective cloud operations. 
+          I excel at implementing DevOps best practices, facilitating collaboration between teams, and ensuring fast and reliable software releases across multiple environments. 
+
+  `,
+  
+
+  experience: [
+    {
+      name: "DevOps Engineer (Remote)",
+      place: "Best Network Systems",
+      location: "Miami, Florida, USA",
+      startDate: "Oct 2023",
+      endDate: "Present",
+      description: [
+        "- Managing CI/CD pipelines with Jenkins, GitLab CI, and AWS CodePipeline for seamless integration.",
+        "- Optimizing AWS resources like EC2, S3, and RDS for cost-effective cloud operations.",
+        "- Automating cloud infrastructure with Terraform, Ansible, and AWS services for efﬁcient deployments.",
+        "- Managing deployment strategies to ensure fast and reliable software releases across multiple environments."
+      ],
+    },
+    {
+      name: "DevOps Engineer",
+      place: "Ceryx Digital",
+      location: "Pune, India",
+      startDate: "Jul 2022",
+      endDate: "Oct 2023",
+      description: [
+        "- Implemented DevOps best practices and facilitated DevOps Collaboration between teams.",
+        "- Assisted in setting up and maintaining development, staging, and production environments.",
+        "- Contributed to continuous integration processes using tools like Jenkins or GitLab CI.",
+        "- Hands-on experience with DevOps tools, scripting and automating CI/CD pipelines.",
+      ],
+    },
+    {
+      name: "Trainee Web Developer",
+      place: "Ceryx Digital",
+      location: "Pune, India",
+      startDate: "Oct 2021",
+      endDate: "Jul 2022",
+      description: [
+        "- Built responsive websites through a hands-on front-end development role.",
+        "- Gained practical experience in modern web technologies and frameworks.",
+        "- Gained exposure to web development workﬂows, collaboration, and best practices.",
+        "- Gained exposure to agile development methodologies and project management tools.",
+      ],
+    },
+    {
+      name: "Web Designer",
+      place: "Ceryx Digital",
+      location: "Amravati, India",
+      startDate: "Mar 2018",
+      endDate: "Nov 2019",
+      description: [
+        "- Designed and developed responsive websites using HTML5, CSS, and JS for cross-browser compatibility.",
+        "- Created user-friendly interfaces and optimized web designs for performance and mobile responsiveness.",
+        "- Managed version control and collaborative development using GitHub for seamless project coordination.",
+        "- Worked closely with teams to deliver custom web solutions, improving user experience and functionality.",
+      ],
+    },
+    // {
+    //   name: "Interns Pakistan",
+    //   place: "",
+    //   location: "Pakistan",
+    //   startDate: "Oct 2020",
+    //   endDate: "Nov 2020",
+    //   description: [
+    //     "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+    //     "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+    //   ],
+    // },
+  ],
+
+  education: [
+    {
+      name: "Brijlal Biyani Science College",
+      location: "Amravati, Maharashtra, India",
+      startDate: "2018",
+      endDate: "2021",
+      description: [
+        "Bachelor of Commerce, focused on finance, accounting and IT.",
+      ],
+    },
+    {
+      name: "Manibai Gujrati High School",
+      location: "Amravati, Maharashtra, India",
+      startDate: "2016",
+      endDate: "2018",
+      description: [
+        "Higher Secondary Education in commerce.",
+      ],
+      // description: ["Intermediate, Pre-Engineering"],
+    },
+    // {
+    //   name: "Ghazi Foundation Schools & Colleges",
+    //   location: "Karāchi, Sindh, Pakistan",
+    //   startDate: "2016",
+    //   endDate: "2018",
+    //   description: ["Matriculation,  Computer Science"],
+    // },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    medium: "https://medium.com/@yashcodedev",
+    github: "https://github.com/philomathaf",
+    email: "mailto:yashcodedev@gmail.com",
+    linkedin: "https://www.linkedin.com/in/yashthakur666/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
